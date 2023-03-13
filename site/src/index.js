6d7d2e18312662152b4f0ebef47b801b6576ca9d
Arthurcmo <arthur_munhoz@hotmail.com>
2023-03-13 15:02:56 -0300

    Add react router-dom

---
+++ site/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { ReactDOM } from "react";
+import ReactDom from 'react-dom'
+import App from './views/App.jsx'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
---
+++ site/src/views/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+  const App = props => (
+    <div className="App">
+        <Router>
+          
+        </Router>
+        
+    </div>
+)
+
+export default App
